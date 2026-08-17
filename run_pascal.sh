5366f755fd6c20edd95e0a1be0642f4e388e6a52
Chen zebin <zebb0528@gmail.com>
2026-08-17 18:47:28 +0800

    Update run_pascal.sh

--- run_pascal.sh
+++ run_pascal.sh
@@ -30,7 +30,6 @@ export CUDA_VISIBLE_DEVICES=5
 
 folder_name=2026-0405_pascal_4task_2dim_${lambda_}_${scheduler}_${lr}_${warmup_step_ratio}_${batch_size}_${accumulate_step}_${nproc_per_node}_${total_epoch}_32_shared_lora_DDP_quad_Tlmd$(date +"%H%M%S_%3N")
 mkdir -p outputs/${folder_name}
-# python -m torch.distributed.launch --nproc_per_node ${nproc_per_node} --master_port 29501 train.py \
 nohup torchrun --nproc_per_node ${nproc_per_node} --master_port 29617 train_pascal.py \
         --type ${type} --lr ${lr} --warmup_step_ratio ${warmup_step_ratio} --total_epoch ${total_epoch} --scheduler ${scheduler} --lambda_ ${lambda_} --use_quad ${use_quad} --tau ${tau} --lmbd ${lmbd} \
         --sam_checkpoint ${sam_checkpoint} --data_root ${data_root} \
